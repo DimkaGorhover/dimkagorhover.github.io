9b19d25f90e9c1f894e6d2d7f73c1cb638119a6e
Dmytro Horkhover <gd.mail.89@gmail.com>
2020-06-09 12:36:16 +0000

    Updates

---
+++ precache-manifest.3286778671c3a969f7788cce1453013e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01436265ce2bfebc8afdabb719b7390f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc73c2dbb4f45efbc39e",
+    "url": "/static/css/main.9ec3f07c.chunk.css"
+  },
+  {
+    "revision": "b8f5615dfe923719dbec",
+    "url": "/static/js/2.42537c83.chunk.js"
+  },
+  {
+    "revision": "cc73c2dbb4f45efbc39e",
+    "url": "/static/js/main.c3e63cc2.chunk.js"
+  },
+  {
+    "revision": "e1ae2030a2600c80004a",
+    "url": "/static/js/runtime~main.150495a4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.368ccec58366d2dbac4284dc88f9cbc6.js"
+  "/precache-manifest.3286778671c3a969f7788cce1453013e.js"
 );
 
 self.addEventListener('message', (event) => {
