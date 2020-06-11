41ba091e44a06742dbf2f75292fee4ffd7d21e97
Dmytro Horkhover <gd.mail.89@gmail.com>
2020-06-11 11:11:15 +0300

    addnew photo

--- src/components/cv/Photo.js
+++ src/components/cv/Photo.js
@@ -4,7 +4,7 @@ import { Image } from 'react-bootstrap';
 export const Photo = () => {
     return (
         <div style={{ alignSelf: 'center' }} >
-            <Image src="/static/photo.png" style={{ width: '100%' }} thumbnail />
+            <Image src="/static/IMG_20190609_191430__01.jpg" style={{ width: '100%' }} thumbnail />
         </div>
     )
 }
